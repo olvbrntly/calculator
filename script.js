dd269f834f94d2066b77051f033aecb1bc834dd5
Olive <olivia.brantley9@gmail.com>
2022-03-21 11:36:48 -0700

    work with all basic operators

--- script.js
+++ script.js
@@ -28,10 +28,6 @@ let calcExponent = (a,b) => a**b;
 
 //OPERATE FUNCTION------------------------------------------
 
-// Create a new function operate that takes an operator and 2 numbers 
-// and then calls one of the above functions on the numbers.
-
-
 function operate(operator, num1, num2,) {
     switch (operator){
         case '+':
@@ -40,119 +36,69 @@ function operate(operator, num1, num2,) {
             return calcSubtract(num1,num2); 
         case '*':
             return calcMultiply(num1, num2);
-        case '÷':
-            return calcDivide(num1,num2);
+        case '/':
+            if(num2===0) return null;
+            else return calcDivide(num1,num2);
         case '%':
             return calcPercentage(num1);
-        case '**':
-            return calcExponent(num1,num2);   
+        case 'x^y':
+            return calcExponent(num1,num2);  
+        default: 
+            return null; 
     }
   };
 
-// Create the functions that populate the display when you click the 
-// number buttons…
-
+//GETS NUMBERS--------------------------------------------------
 numberButton.forEach((number)=>{
-    number.addEventListener('click', function(){
-       if(num1 === ''){
+    number.addEventListener('click', function(){ 
+        if(chosenOperator === ""){ 
         num1 += number.textContent;
         display.textContent = num1;
         console.log('num1: ' + num1);   
-    }else {
+        } else {
         num2 += number.textContent;
-        display.textContent = num2;
-        console.log('num2:' + num2);
-    }
+        display.textContent = num1 +chosenOperator + num2;
+        console.log('num2: ' + num2);     
+        }
     })
-    
 });
 
+//GETS OPERATOR----------------------------------------------------
 operatorButton.forEach((operator)=>{
     operator.addEventListener('click', function(){
-        choosingNumber = false;
         chosenOperator = operator.textContent;
-        display.textContent += chosenOperator
-        console.log(chosenOperator);
+        if(chosenOperator === 'x^y'){
+            display.textContent = num1 + "^";
+        }else if(chosenOperator === '%'){
+            calculate();
+        }else{
+        display.textContent = num1 + chosenOperator;
+        console.log(`operator: ${chosenOperator}`);
+        }
     })
     return chosenOperator;
 });
 
+//CALCULATES USING FUNCTIONS ABOVE---------------------------------
 function calculate() {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    let result = operate(chosenOperator, num1, num2);
-    display.textContent = result;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    let result = operate(chosenOperator, num1, num2)
+    display.textContent =result
     num1 = result;
     num2 ='';
     console.log(result);
     
 };
 
-function clear() {
-    num1 = "";
-    num2 = "";
-    chosenOperator = "";
-    display.textContent = "";
-
-};
-
-
-
 equalButton.addEventListener('click', calculate);
 
 clearButton.addEventListener('click', clear);
 
 
-
-
-
-
-
-
-
-
-//you should be storing the ‘display value’ in a variable 
-// somewhere for use in the next step.
-
-
-
-
-
-
-
-
-
-// Make the calculator work! You’ll need to store the first number that is 
-// input into the calculator when a user presses an operator, and also save 
-// which operation has been chosen and then operate() on them when the user 
-// presses the “=” key.
-// You should already have the code that can populate the display, so once 
-// operate() has been called, update the display with the ‘solution’ to the 
-// operation.
-// This is the hardest part of the project. You need to figure out how to
-//  store all the values and call the operate function with them. Don’t 
-//  feel bad if it takes you a while to figure out the logic
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
+function clear() {
+    num1 = "";
+    num2 = "";
+    chosenOperator = "";
+    display.textContent = "";
+};
